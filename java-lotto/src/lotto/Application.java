9401fd15b9ee053b2c8e6c5912b36e4d5bcd2556
minkyuu <minkyu023@gmail.com>
2021-08-22 17:15:48 +0900

    feat: add isValueInThousands()
    This method is for preventing unexpected input
    
    - if input digit dosen't divide by 1000, return false.
    
    - other cases return true

--- java-lotto/src/lotto/Application.java
+++ java-lotto/src/lotto/Application.java
@@ -11,10 +11,22 @@ public class Application {
         String price = sc.nextLine();
 
         boolean numCheck = isDigit(price);
+        if (numCheck) {
+            boolean valueCheck = isValuesInThousands(price);
+        }
+
     }
 
     public static boolean isDigit(String number) {
         String pattern = "^[0-9]*$";
         return Pattern.matches(pattern, number);
     }
+
+    public static boolean isValuesInThousands(String price) {
+        int num = Integer.parseInt(price);
+        if ((num % 1000) == 0)
+            return true;
+        return false;
+    }
+
 }
