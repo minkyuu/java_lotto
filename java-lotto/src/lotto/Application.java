6009549dcc5681b7c57e1b37b55117395188740a
minkyuu <minkyu023@gmail.com>
2021-08-22 16:46:58 +0900

    feat: add isDigit()
    This method can handle exceptional input like string, minus digit.
    
    (ex)
    input : "asdlq"
    output : false
    
    input : "1234"
    output : true

--- java-lotto/src/lotto/Application.java
+++ java-lotto/src/lotto/Application.java
@@ -1,11 +1,21 @@
 package lotto;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Application {
 
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String price = sc.nextLine();
 
-
+        boolean numCheck = isDigit(price);
 
     }
 
+    public static boolean isDigit(String number) {
+        String pattern = "^[0-9]*$";
+        return Pattern.matches(pattern, number);
+    }
 }
